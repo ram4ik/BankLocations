bdac4a32fc0a550510d10cd9470e3626dc5bf6ba
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-10-07 19:33:31 +0300

    Initial Commit

---
+++ BankLocations/BankLocationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankLocationsApp.swift
+//  BankLocations
+//
+//  Created by ramil on 07.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct BankLocationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
